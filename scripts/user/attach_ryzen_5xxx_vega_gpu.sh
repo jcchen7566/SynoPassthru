5e9cbf05339fc83d10b8ce38a6abc131cea943fe
jc.chen <james35022000@gmail.com>
2025-04-02 04:32:51 +0800

    support passing through AMD Ryzen 5xxx iGPU, Audio and USB controller
    Already tested on Windows 11 VM, with AMD Ryzen 5350GE + Asrock B450M Pro4(BIOS
    L5.39 AGESA 1.2.0.7)

---
+++ scripts/user/attach_ryzen_5xxx_vega_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# The PCI device you want to attach or detach
+ADDRESS="08:00.0" # Remain empty to attach/detach to all VENDOR:PRODUCT devices
+VENDOR="1002"
+PRODUCT="1638"
+ROM="/usr/local/libvirt/user/vbios_1002_1638.bin"
+
+# The VM you want to attach or detach (from "virsh list")
+VM_NAME="591328d9-88d5-42aa-9551-bb793c3330cf"
+export VM_NAME
+
+if [ "$TARGET_VM" == "$VM_NAME" ]; then
+    /usr/local/libvirt/attach_detach_pci_from_vm.sh "ATTACH" "$ADDRESS" "$VENDOR" "$PRODUCT" "$ROM"
+fi
